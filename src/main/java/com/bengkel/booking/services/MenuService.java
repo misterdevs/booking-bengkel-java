2f47264b5927de97c0daa5d9ad44f91fb24d4b4b
M Ridho <ridhomuhammad.rb@gmail.com>
2024-05-31 13:08:39 +0700

    feat: add menu functionality

--- src/main/java/com/bengkel/booking/services/MenuService.java
+++ src/main/java/com/bengkel/booking/services/MenuService.java
@@ -4,8 +4,10 @@ import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.bengkel.booking.models.BookingOrder;
 import com.bengkel.booking.models.Customer;
 import com.bengkel.booking.models.ItemService;
+import com.bengkel.booking.models.Vehicle;
 import com.bengkel.booking.repositories.CustomerRepository;
 import com.bengkel.booking.repositories.ItemServiceRepository;
 import com.bengkel.booking.utilities.UtilityInput;
@@ -14,10 +16,13 @@ import com.bengkel.booking.utilities.UtilityMenu;
 public class MenuService {
 	private static List<Customer> listAllCustomers = CustomerRepository.getAllCustomer();
 	private static List<ItemService> listAllItemService = ItemServiceRepository.getAllItemService();
+	private static List<BookingOrder> listAllBookingOrder = new ArrayList<BookingOrder>();
 	private static UtilityMenu menu = new UtilityMenu();
 	private static UtilityInput input = new UtilityInput();
-	private static String appName = "Booking Bengkel by MRDevs";
+	private static String appName = "Booking Bengkel";
+	private static String appSubName = "by MRDevs";
 
+	private static Customer loggedInCustomer = null;
 	private static boolean isLogin = false;
 	private static int numbOfAttempts = 1;
 	private static int suspendTime = 10; // in second
@@ -29,15 +34,22 @@ public class MenuService {
 		UtilityMenu menu = new UtilityMenu();
 		String[] loginMenuArr = { "Login" };
 
+		// FOR DEVELOPMENT ONLY
+		// isLogin = true;
+		// loggedInCustomer = listAllCustomers.get(2);
+		// mainMenu();
+		// FOR DEVELOPMENT ONLY
+
 		menu.createMenu(chosenMenu -> {
 			AuthService.removeExpiredSuspend(suspendedCustomers);
 			login(chosenMenu);
 			if (isLogin) {
 				mainMenu();
 				isLogin = false;
+				loggedInCustomer = null;
 			}
 
-		}, appName, loginMenuArr, 0, "Exit");
+		}, appSubName, appName, loginMenuArr, 0, "Exit");
 
 		System.out.println("==========================================");
 		System.out.println("APPLICATION HAS BEEN CLOSED");
@@ -47,10 +59,8 @@ public class MenuService {
 
 	public static void login(String chosenMenu) {
 		menu.resetDisplay();
-		System.out.println(menu.displayBorder);
-		System.out.println("Login - " + appName);
-		System.out.println(menu.displayBorder);
-
+		menu.printTitleCustom("Login", 1, 0);
+		menu.printTitleCustom("Booking Bengkel", 0, 1);
 		if (Integer.valueOf(chosenMenu) != 0) {
 			numbOfAttempts = 1;
 			isLogin = true;
@@ -70,7 +80,7 @@ public class MenuService {
 				input.validate("Masukkan Password", "Password salah!",
 						pass -> {
 							boolean isValid = Validation
-									.isValidAuth(UserService.getCustomerById(listAllCustomers, customerId), pass);
+									.isValidAuth(CustomerService.getCustomerById(listAllCustomers, customerId), pass);
 							if (Validation.isReachedLimitAttemptsAuth(numbOfAttempts) && !isValid) {
 								isLogin = false;
 								return true;
@@ -79,7 +89,9 @@ public class MenuService {
 							return isValid;
 						});
 
-				if (Validation.isReachedLimitAttemptsAuth(numbOfAttempts) && !isLogin) {
+				if (isLogin) {
+					loggedInCustomer = CustomerService.getCustomerById(listAllCustomers, customerId);
+				} else if (Validation.isReachedLimitAttemptsAuth(numbOfAttempts) && !isLogin) {
 					suspendUntil = LocalTime.now().plusSeconds(suspendTime);
 					suspendedCustomers.add(new Object[] { customerId, suspendUntil });
 					System.out.println("Percobaan sudah 3 kali. Coba lagi dalam " + suspendTime + " detik.");
@@ -93,23 +105,32 @@ public class MenuService {
 
 	public static void mainMenu() {
 		String[] mainMenuArr = { "Informasi Customer", "Booking Bengkel", "Top Up Bengkel Coin", "Informasi Booking" };
-		menu.createMenu(a -> handleMainMenu(a), appName, mainMenuArr, 0, "Logout");
+		menu.createMenu(a -> handleMainMenu(a), appSubName, appName, mainMenuArr, 0, "Logout");
 	}
 
 	public static void handleMainMenu(String chosenMenu) {
+
 		menu.resetDisplay();
 		switch (Integer.valueOf(chosenMenu)) {
 			case 1:
 				// panggil fitur Informasi Customer
+
+				PrintService.showCustomerProfile(loggedInCustomer);
+				menu.enterToContinue();
 				break;
 			case 2:
 				// panggil fitur Booking Bengkel
+				PrintService.showBooking(listAllBookingOrder, listAllItemService, loggedInCustomer);
+				menu.enterToContinue();
 				break;
 			case 3:
 				// panggil fitur Top Up Saldo Coin
+				PrintService.showTopupSaldo(loggedInCustomer);
 				break;
 			case 4:
 				// panggil fitur Informasi Booking Order
+				PrintService.showBookingOrder(listAllBookingOrder);
+				menu.enterToContinue();
 				break;
 		}
 	}
